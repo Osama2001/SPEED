4649d3b6c1738b3bb711c7543cd2baf6468a8ed5
Osama2001 <40809896+Osama2001@users.noreply.github.com>
2018-07-17 08:25:52 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,78 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const bot = new Discord.Client();
+var moment = require('moment')
+const fs = require('fs');
+const cooldown = new Set();
+var moment = require('moment')
+var Canvas = require('canvas');
+var jimp = require('jimp');
+const prefix = "+";
+const embed = new Discord.RichEmbed()
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+      console.log('----------------');
+    console.log('By Osama_DK');
+    console.log('----------------');
+});
+
+client.on('ready', () => {
+    client.user.setGame(`SPEEDXCRAFT`, "http://twitch.tv/Streammingg")	
+});
+
+
+
+ client.on('message', message => {
+if(message.content.startsWith(prefix +'sug')) {
+if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+      const A8tra7Room = message.guild.channels.find("name", "suggestions")
+      if(!message.channel.guild) return message.reply(`هذا الأمر فقط ل السيرفرات :x:`);
+   let a8tra7 = message.content.split(' ').slice(1).join(' ')
+   var m8tr7 = message.author.id
+if(!message.guild.channels.find("name","suggestions")) return message.channel.send('انشء روم باسم   \`suggestions \`')
+   var ThxForSug = new Discord.RichEmbed()
+   .setTitle(`:white_check_mark: Success!`)
+   .setTimestamp()
+   .setDescription(`شكراً على اقتراحك !`)
+.setDescription(`**Your Suggestion** : ${a8tra7}`)
+   var Sure = new Discord.RichEmbed()
+   .setTimestamp()
+   .setTitle(`هل انت متأكد من ارسال الاقتراح؟ معك دقيقه قبل الالغاء`)
+.setDescription(`Suggestion : **${a8tra7}**`)
+		 .setFooter('DK SUPPORT' , client.user.avatarURL)
+message.channel.sendEmbed(Sure).then(msg => {
+    msg.react('❎')
+.then(() => msg.react('✅'))
+
+let YesFilter = (reaction, user) => reaction.emoji.name === '✅'  && user.id === message.author.id;
+let NoFilter = (reaction, user) => reaction.emoji.name === '❎' && user.id === message.author.id;
+
+let Yes = msg.createReactionCollector(YesFilter, { time: 60000 });
+let No = msg.createReactionCollector(NoFilter, { time: 60000 });
+
+Yes.on("collect", r => {
+   var ala8tra7 = new Discord.RichEmbed()
+   .setTimestamp()
+   .setColor('RANDOM')
+   .setThumbnail(message.author.avatarURL)
+   .setTitle(`New Suggestion :bell:`)
+   .setDescription(`From : __<@${m8tr7}>__\n\nSuggestion: **${a8tra7}**`)
+   .setFooter(`${message.author.username}#${message.author.discriminator}`)
+   A8tra7Room.send(ala8tra7)
+   message.channel.sendEmbed(ThxForSug).then(message => {message.delete(6000)})
+msg.delete();
+})
+No.on("collect", r => {
+message.channel.send('تم الغاء اقتراحط بنجاح :white_check_mark: ').then(message => {message.delete(4000)})
+msg.delete();
+})
+})
+}
+});   
+
+
+
+
+
+
+   client.login(process.env.BOT_TOKEN);
