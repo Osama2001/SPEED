afa71ccfe544f1c5f991611269519d69c4f71456
Osama2001 <40809896+Osama2001@users.noreply.github.com>
2018-07-20 02:11:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,22 +24,22 @@ client.on('message', message => {
       
   if (message.content.startsWith(adminprefix + 'setplaying')) {
     client.user.setGame(argresult);
-      message.channel.send(`**Done You Have Been Changed The Playing To ${argresult}**✅ `)
+      message.channel.send(`Done You Have Been Changed The Playing To ${argresult}**✅ `)
   } else 
      if (message.content === (adminprefix + "dkbotleave")) {
     message.guild.leave();        
   } else  
   if (message.content.startsWith(adminprefix + 'setwatching')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**Done You Have Been Changed The Watching To **${argresult}**✅`)
+      message.channel.send(`Done You Have Been Changed The Watching To **${argresult}**✅`)
   } else 
   if (message.content.startsWith(adminprefix + 'setlistening')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**Done You Have Been Changed The Listening To  **${argresult}**✅`)
+      message.channel.send(`Done You Have Been Changed The Listening To  **${argresult}**✅`)
   } else 
   if (message.content.startsWith(adminprefix + 'setstreaming')) {
     client.user.setGame(argresult, "https://www.twitch.tv/idk");
-      message.channel.send(`**Done You Have Been Changed The Streaming To **${argresult}**✅`)
+      message.channel.send(`Done You Have Been Changed The Streaming To **${argresult}**✅`)
   }
   if (message.content.startsWith(adminprefix + 'setname')) {
   client.user.setUsername(argresult).then
